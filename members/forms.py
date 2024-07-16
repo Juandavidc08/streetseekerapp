d00f6fbd5451ec08162c9d69de25a72dbddd8574
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-07-16 10:49:25 +0000

    Add widget to style registration form

--- members/forms.py
+++ members/forms.py
@@ -3,11 +3,19 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
+    #add widget to style the django form 
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name','email', 'password1', 'password2')
+    #add widgets for username and passwords to style it better with bootstrap
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
 
