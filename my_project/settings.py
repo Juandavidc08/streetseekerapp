8ef525deec4c59b16ab9c08cab8cff71254d5b56
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-07-19 13:25:24 +0000

    Deployment test

--- my_project/settings.py
+++ my_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-juandavidc0-streetseeke-usm8pwbg55e.ws.codeinstitute-ide.net','.herokuapp.com']
 
